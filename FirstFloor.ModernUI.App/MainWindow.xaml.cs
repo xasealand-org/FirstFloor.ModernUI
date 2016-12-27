c3d58267a90d3c3f67ed20e8c25f641b959a755b
wgx <656052658@qq.com>
2016-12-27 21:36:31 +0800

    汉化资源文件

--- FirstFloor.ModernUI.App/MainWindow.xaml.cs
+++ FirstFloor.ModernUI.App/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FirstFloor.ModernUI.App
 {
@@ -20,9 +21,34 @@ namespace FirstFloor.ModernUI.App
     /// </summary>
     public partial class MainWindow : ModernWindow
     {
+        //private DispatcherTimer timer;
+        //public double EndTop { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //timer = new DispatcherTimer();
+            //timer.Interval = TimeSpan.FromMilliseconds(300);
+            //timer.Tick += timer_Tick;
+            //this.Left = SystemParameters.WorkArea.Width - this.Width;
+            //this.EndTop = SystemParameters.WorkArea.Height - this.Height;
+            //this.Top = SystemParameters.WorkArea.Height;
+
+            //this.Loaded += MainWindow_Loaded;
         }
+
+        //private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    timer.Start();
+        //}
+
+        //void timer_Tick(object sender, EventArgs e)
+        //{
+        //    while (this.Top > EndTop)
+        //    {
+        //        this.Top -= 5;
+        //    }
+        //}
     }
 }
