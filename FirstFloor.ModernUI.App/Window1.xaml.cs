4ed5371af5e5338a5e390bc8d90283a5d21e6434
wgx <656052658@qq.com>
2016-12-27 22:34:18 +0800

    新增下拉列表显示英文和中文字体

--- FirstFloor.ModernUI.App/Window1.xaml.cs
+++ FirstFloor.ModernUI.App/Window1.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -22,6 +23,29 @@ namespace FirstFloor.ModernUI.App
         public Window1()
         {
             InitializeComponent();
+        }
+
+        public void win_LoadedEvent(object sender, RoutedEventArgs e)
+        {
+            foreach (FontFamily _f in Fonts.SystemFontFamilies)
+            {
+                LanguageSpecificStringDictionary _fontDic = _f.FamilyNames;
+                string _fontName = null;
+                if (_fontDic.ContainsKey(XmlLanguage.GetLanguage("zh-cn")))
+                {
+                    if (_fontDic.TryGetValue(XmlLanguage.GetLanguage("zh-cn"), out _fontName))
+                    {
+                        cbo_Demo.Items.Add(_fontName);
+                    }
+                }
+                else if (_fontDic.ContainsKey(XmlLanguage.GetLanguage("en-us")))
+                {
+                    if (_fontDic.TryGetValue(XmlLanguage.GetLanguage("en-us"), out _fontName))
+                    {
+                        cbo_Demo.Items.Add(_fontName);
+                    }
+                }
+            }
         }
     }
 }
