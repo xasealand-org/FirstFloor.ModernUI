1f5549d4853850bbdb4ca986a7b2f4b3fe818fb0
wgx <656052658@qq.com>
2016-12-28 23:55:30 +0800

    画登陆页面参考第三方控件

---
+++ Shared/Windows/Controls/ControlAttachProperty.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using TextBox = System.Windows.Controls.TextBox;
+//using TextBoxBase = System.Windows.Controls.Primitives.TextBoxBase;
+
+namespace FirstFloor.ModernUI.Windows.Controls
+{
+    /// <summary>
+    /// 控件附加属性
+    /// </summary>
+    public class ControlAttachProperty
+    {
+        /// <summary>
+        /// 静态构造函数
+        /// </summary>
+        static ControlAttachProperty()
+        {
+            //ClearTextCommand
+            //ClearTextCommand = new RoutedUICommand();
+            //ClearTextCommandBinding = new CommandBinding(ClearTextCommand);
+            //ClearTextCommandBinding.Executed += ClearButtonClick;
+            ////OpenFileCommand
+            //OpenFileCommand = new RoutedUICommand();
+            //OpenFileCommandBinding = new CommandBinding(OpenFileCommand);
+            //OpenFileCommandBinding.Executed += OpenFileButtonClick;
+            ////OpenFolderCommand
+            //OpenFolderCommand = new RoutedUICommand();
+            //OpenFolderCommandBinding = new CommandBinding(OpenFolderCommand);
+            //OpenFolderCommandBinding.Executed += OpenFolderButtonClick;
+
+            //SaveFileCommand = new RoutedUICommand();
+            //SaveFileCommandBinding = new CommandBinding(SaveFileCommand);
+            //SaveFileCommandBinding.Executed += SaveFileButtonClick;
+        }
+
+        #region FocusBorderBrush 焦点边框色，输入控件
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty FocusBorderBrushProperty = DependencyProperty.RegisterAttached(
+            "FocusBorderBrush", typeof(Brush), typeof(ControlAttachProperty), new FrameworkPropertyMetadata(null));
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="value"></param>
+        public static void SetFocusBorderBrush(DependencyObject element, Brush value)
+        {
+            element.SetValue(FocusBorderBrushProperty, value);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static Brush GetFocusBorderBrush(DependencyObject element)
+        {
+            return (Brush)element.GetValue(FocusBorderBrushProperty);
+        }
+
+        #endregion
+
+        #region MouseOverBorderBrush 鼠标进入边框色，输入控件
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty MouseOverBorderBrushProperty =
+            DependencyProperty.RegisterAttached("MouseOverBorderBrush", typeof(Brush), typeof(ControlAttachProperty),
+                new FrameworkPropertyMetadata(Brushes.Transparent,
+                    FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.Inherits));
+
+        /// <summary>
+        /// Sets the brush used to draw the mouse over brush.
+        /// </summary>
+        public static void SetMouseOverBorderBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(MouseOverBorderBrushProperty, value);
+        }
+
+        /// <summary>
+        /// Gets the brush used to draw the mouse over brush.
+        /// </summary>
+        [AttachedPropertyBrowsableForType(typeof(TextBox))]
+        [AttachedPropertyBrowsableForType(typeof(CheckBox))]
+        [AttachedPropertyBrowsableForType(typeof(RadioButton))]
+        [AttachedPropertyBrowsableForType(typeof(DatePicker))]
+        [AttachedPropertyBrowsableForType(typeof(ComboBox))]
+        [AttachedPropertyBrowsableForType(typeof(RichTextBox))]
+        public static Brush GetMouseOverBorderBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(MouseOverBorderBrushProperty);
+        }
+
+        #endregion
+
+        #region WatermarkProperty 水印
+        /// <summary>
+        /// 水印
+        /// </summary>
+        public static readonly DependencyProperty WatermarkProperty = DependencyProperty.RegisterAttached(
+            "Watermark", typeof(string), typeof(ControlAttachProperty), new FrameworkPropertyMetadata(""));
+
+        /// <summary>
+        /// 获取水印
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static string GetWatermark(DependencyObject d)
+        {
+            return (string)d.GetValue(WatermarkProperty);
+        }
+        /// <summary>
+        /// 设置水印
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetWatermark(DependencyObject obj, string value)
+        {
+            obj.SetValue(WatermarkProperty, value);
+        }
+        #endregion
+
+        #region LabelProperty TextBox的头部Label
+        /// <summary>
+        /// TextBox的头部Label
+        /// </summary>
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.RegisterAttached(
+            "Label", typeof(string), typeof(ControlAttachProperty), new FrameworkPropertyMetadata(null));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        [AttachedPropertyBrowsableForType(typeof(TextBox))]
+        public static string GetLabel(DependencyObject d)
+        {
+            return (string)d.GetValue(LabelProperty);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetLabel(DependencyObject obj, string value)
+        {
+            obj.SetValue(LabelProperty, value);
+        }
+        #endregion
+
+        #region LabelTemplateProperty TextBox的头部Label模板
+        /// <summary>
+        /// TextBox的头部Label模板
+        /// </summary>
+        public static readonly DependencyProperty LabelTemplateProperty = DependencyProperty.RegisterAttached(
+            "LabelTemplate", typeof(ControlTemplate), typeof(ControlAttachProperty), new FrameworkPropertyMetadata(null));
+
+        /// <summary>
+        /// 获取Label模板
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        [AttachedPropertyBrowsableForType(typeof(TextBox))]
+        public static ControlTemplate GetLabelTemplate(DependencyObject d)
+        {
+            return (ControlTemplate)d.GetValue(LabelTemplateProperty);
+        }
+        /// <summary>
+        /// 设置Label模板
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetLabelTemplate(DependencyObject obj, ControlTemplate value)
+        {
+            obj.SetValue(LabelTemplateProperty, value);
+        }
+        #endregion
+
+        #region AttachContentProperty 附加组件模板
+        /// <summary>
+        /// 附加组件模板
+        /// </summary>
+        public static readonly DependencyProperty AttachContentProperty = DependencyProperty.RegisterAttached(
+            "AttachContent", typeof(ControlTemplate), typeof(ControlAttachProperty), new FrameworkPropertyMetadata(null));
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static ControlTemplate GetAttachContent(DependencyObject d)
+        {
+            return (ControlTemplate)d.GetValue(AttachContentProperty);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetAttachContent(DependencyObject obj, ControlTemplate value)
+        {
+            obj.SetValue(AttachContentProperty, value);
+        }
+        #endregion
+
+        #region CornerRadiusProperty Border圆角
+        /// <summary>
+        /// Border圆角
+        /// </summary>
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "CornerRadius", typeof(CornerRadius), typeof(ControlAttachProperty), new FrameworkPropertyMetadata(null));
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static CornerRadius GetCornerRadius(DependencyObject d)
+        {
+            return (CornerRadius)d.GetValue(CornerRadiusProperty);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+        #endregion
+    }
+}
